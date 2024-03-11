1d69815fab88f74a4e7b14ae1fc17f45fe34e7ba
Summist <99487908+Summist@users.noreply.github.com>
2024-03-11 15:02:00 +0300

    Правки в файле Program, добавление ссылок на проекты Domain, Infrustructure.SqlServer, Application.

--- EdgeItalianPizza.WinForms.Client/Program.cs
+++ EdgeItalianPizza.WinForms.Client/Program.cs
@@ -1,3 +1,5 @@
+using EdgeItalianPizza.WinForms.Client.Forms.Auth;
+
 namespace EdgeItalianPizza.WinForms.Client;
 
 internal static class Program
@@ -11,6 +13,6 @@ internal static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        System.Windows.Forms.Application.Run(new Form1());
+        System.Windows.Forms.Application.Run(new AuthorizationForm());
     }
 }
--- EdgeItalianPizza.WinForms.Courier/Program.cs
+++ EdgeItalianPizza.WinForms.Courier/Program.cs
@@ -1,3 +1,5 @@
+using EdgeItalianPizza.WinForms.Courier.Forms.Auth;
+
 namespace EdgeItalianPizza.WinForms.Courier;
 
 internal static class Program
@@ -11,6 +13,6 @@ internal static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        System.Windows.Forms.Application.Run(new AuthorizationForm());
     }
 }
